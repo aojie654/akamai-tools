02c9b46d42c423537dec18479d5491826552a9c8
aojie654 <aojie654@live.cn>
2022-07-28 15:17:17 +0800

    cip version 0.2.3

--- 01_cip/bin/cip.py
+++ 01_cip/bin/cip.py
@@ -503,7 +503,7 @@ def update():
 
 if __name__ == "__main__":
     # _DEBUG_FLAG
-    sys.argv = [__file__, "-i", "1.1.1.1"]
+    # sys.argv = [__file__, "-i", "1.1.1.1"]
     # sys.argv = [__file__, "-i", "1.1.1.1", "www.akamai.com"]
     # sys.argv = [__file__, "-i", "1.1.1.1", "www.akamai.com", "-d", "8.8.8.8"]
     # sys.argv = [__file__, "-f", "/Users/sao/Downloads/iptest1.txt"]
