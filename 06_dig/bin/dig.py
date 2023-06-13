f47cef51a2a57f0da03220b0bc2c6908fed874f8
aojie654 <aojie654@live.cn>
2023-06-13 09:44:41 +0800

    Initial 1st version

---
+++ 06_dig/bin/dig.py
@@ -0,0 +1,303 @@
+# encoding=utf-8
+
+"""
+dig with multiple DNS
+通过多个 DNS 进行解析
+"""
+
+import argparse
+import csv
+import json
+from pathlib import Path
+
+from dns import resolver
+
+# Initial path of root, filename and path of input
+path_root = Path(__file__).parent
+path_conf_folder = path_root.parent.joinpath("conf")
+filename_dns = "dns.json"
+path_dns_input = path_conf_folder.joinpath(filename_dns)
+path_input_folder = path_conf_folder
+filename_hostnames_input = "input_hostnames.txt"
+path_hostnames_input = path_input_folder.joinpath(filename_hostnames_input)
+
+
+def get_dns():
+    # Initial a dict of result and read config
+    dns_dict = dict()
+    with open(file=path_dns_input, mode="r", encoding="utf-8", errors="ignore") as dns_file_obj:
+        dns_json = dns_file_obj.read()
+        dns_dict = json.loads(dns_json)
+
+    return dns_dict
+
+
+def get_hostnames():
+
+    hostnames_list = list()
+
+    # Open input file, read the hostnames to list
+    with open(file=path_hostnames_input, mode="r", encoding="utf-8", errors="ignore") as obj_hostnames_input:
+        hostnames_list = obj_hostnames_input.readlines()
+        # Remove the \n of line
+        index_tmp = 0
+        for index_tmp in range(len(hostnames_list)):
+            hostname_tmp = hostnames_list[index_tmp].replace("\n", "")
+            hostnames_list[index_tmp] = hostname_tmp
+
+    return hostnames_list
+
+
+def process_input_std(hostnames, dns_type="A"):
+    # Resolve the hostnames
+    output_result_hostnames = dict()
+
+    for hostname in hostnames:
+        output_result_hostnames[hostname] = resolve_hostname(hostname=hostname, dns_type=dns_type)
+
+    return output_result_hostnames
+
+
+def process_input_files(files, dns_type="A"):
+    output_result_files = "Result of files: {:}".format(files)
+
+    return output_result_files
+
+
+def resolve_hostname(hostname, dns_type):
+    try:
+        # Get DNS dict
+        dns_dict = get_dns()
+        output_result_hostname = dict()
+        dns_resolver = resolver.Resolver()
+        for dns_server in dns_dict.keys():
+            print_msg = "Working on hostname: {:} of DNS: {:}, ".format(hostname, dns_server)
+            print(print_msg, end="")
+            output_result_hostname[dns_server] = dns_dict[dns_server]
+            dns_resolver.nameservers = [dns_server]
+            try:
+                dns_response = dns_resolver.resolve(qname=hostname, rdtype=dns_type, lifetime=1)
+                if dns_response.rrset:
+                    dns_resolve_result = dns_response.rrset.to_text().split()[-1]
+            except Exception as expt:
+                dns_resolve_result = "Exception: {:}".format(str(expt))
+            output_result_hostname[dns_server]["Result"] = dns_resolve_result
+            print_msg = "result: {:}".format(dns_resolve_result)
+            print(print_msg)
+    except Exception as expt:
+        output_result_hostname = {
+            "Exception": "{:}, {:}".format(hostname, expt),
+        }
+
+    return output_result_hostname
+
+
+def resolve_hostname_by_dns(hostname, dns):
+    resolve_hostname_result = dict()
+
+    return resolve_hostname_result
+
+
+def output_hostname(hostname):
+    # def output_hostname(hostname, output):
+
+    # path_output_folder = path_conf_folder
+    # filename_hostnames_output_csv = "output_{:}.csv".format(hostname)
+    # path_hostnames_output_csv = path_output_folder.joinpath(filename_hostnames_output_csv)
+    # filename_hostnames_output_txt = "output_{:}.txt".format(hostname)
+    # path_hostnames_output_txt = path_output_folder.joinpath(filename_hostnames_output_txt)
+
+    # try:
+    #     output_result = "finished"
+    # except Exception as exception_t:
+    #     output_result = str(exception_t)
+
+    output_result = "Output to STR output: {:}".format(hostname)
+
+    return output_result
+
+
+def output_hostname_csv(hostname):
+    # def output_hostname_csv(hostname, output):
+    # path_output_folder = path_input_folder
+    # filename_hostnames_output = "{:}_output.csv"
+    # csv_header = [
+    #     "DNS",
+    #     "Location",
+    #     "Provider",
+    #     "Result",
+    # ]
+    # csv_writer = csv.DictWriter(fieldnames=csv_header, delimiter="|", quotechar="\"")
+    # for key_tmp in output.keys():
+    #     csv_writer.writerow(
+    #         {
+    #             csv_header[0]: output[key_tmp],
+    #         }
+    #     )
+    output_result = "Output as CSV: {:}".format(hostname)
+
+    # return
+    return output_result
+
+
+def output_hostname_txt(hostname):
+    # def output_hostname_txt(hostname, output):
+
+    output_result = "Output as TXT: {:}".format(hostname)
+
+    return output_result
+
+
+def output_hostname_txt_deduplicate(output_result):
+
+    print("Deduplicate values in txt.")
+
+    return output_result
+
+
+def output_hostname_json(hostname):
+    # def output_hostname_json(hostname, output):
+
+    output_result = "Output as JSON: {:}".format(hostname)
+
+    return output_result
+
+
+def process_output(output_result, output_formats, output_deduplicated=False):
+    """
+    output result like:
+    {
+        "csv": {
+            "www.akamai.com": [
+                "DNS: 1.1.1.1, Location: US, Provider: CloudFlare.com, Result: 23.23.23.23",
+                "DNS: 1.1.1.2, Location: US, Provider: CloudFlare.com, Result: 23.23.23.24"
+            ],
+            "www.akamai.cn": [
+                "DNS: 1.1.1.1, Location: US, Provider: CloudFlare.com, Result: 182.91.91.91",
+                "DNS: 1.1.1.2, Location: US, Provider: CloudFlare.com, Result: 182.91.91.92"
+            ]
+        },
+        "txt": {
+            "www.akamai.com": "23.23.23.23, 23.23.23.24",
+            "www.akamai.cn": "182.91.91.91, 182.91.91.92"
+        },
+        "json": {
+            "www.akamai.com": {
+                "1.1.1.1": {
+                    "DNS": "1.1.1.1",
+                    "Location": "US",
+                    "Provider": "CloudFlare.com",
+                    "Result": "23.23.23.23"
+                },
+                "1.1.1.2": {
+                    "DNS": "1.1.1.2",
+                    "Location": "US",
+                    "Provider": "CloudFlare.com",
+                    "Result": "23.23.23.24"
+                }
+            },
+            "www.akamai.cn": {
+                "1.1.1.1": {
+                    "DNS": "1.1.1.1",
+                    "Location": "US",
+                    "Provider": "CloudFlare.com",
+                    "Result": "182.91.91.91"
+                },
+                "1.1.1.2": {
+                    "DNS": "1.1.1.2",
+                    "Location": "US",
+                    "Provider": "CloudFlare.com",
+                    "Result": "182.91.91.92"
+                }
+            }
+        }
+    }
+    """
+    output_result_dict = dict()
+
+    # Output with format
+    if "csv" in output_formats:
+        # Initial the format with csv
+        output_result_dict["csv"] = output_result
+    if "txt" in output_formats:
+        output_result_txt = dict()
+        for hostname in output_result.keys():
+            output_list_txt = list()
+            output_result_txt[hostname] = str()
+            for dns_server in output_result[hostname].keys():
+                    if ((output_result[hostname][dns_server]["Result"] in output_list_txt) and output_deduplicated):
+                        continue
+                    else:
+                        output_result_txt[hostname] = "{:}{:}, ".format(output_result_txt[hostname], output_result[hostname][dns_server]["Result"])
+                        output_list_txt.append(output_result[hostname][dns_server]["Result"])
+                # output_result_txt[hostname] = output_hostname_content_txt
+        output_result_dict["txt"] = output_result_txt
+    if "json" in output_formats:
+        output_result_json = output_result
+        output_result_dict["json"] = output_result_json
+
+    return output_result_dict
+
+
+def process_output_std(output_result, output_formats, output_deduplicate=False):
+
+    output_result = process_output(output_result, output_formats=output_formats, output_deduplicated=output_deduplicate)
+    if "csv" in output_formats:
+        output_result_csv = dict()
+        for hostname in output_result["csv"].keys():
+            output_result_csv[hostname] = list()
+            for output_key in output_result["csv"][hostname].keys():
+                    output_dict_t = output_result["csv"][hostname][output_key]
+                    output_hostname_content_csv = "DNS: {:}, Location: {:}, Provider: {:}, Result: {:}".format(
+                        output_key, output_dict_t["Location"], output_dict_t["Provider"], output_dict_t["Result"])
+                    output_result_csv[hostname].append(output_hostname_content_csv)
+        output_result["csv"] = output_result_csv
+    output_result_json = json.dumps(output_result, ensure_ascii=False, indent=4)
+    print_msg = """
+    ====> Result:
+    {:}
+    """.format(output_result_json)
+    print(print_msg)
+
+
+def process_output_file(output_result, output_formats, output_deduplicate=False):
+
+    save_result = output_formats
+
+    return save_result
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(prog="akdig", description="Resolve the hostnames with multiple DNS.")
+    arg_parser.add_argument("-i", "--inputs", type=str, nargs="+", help="Use hostnames as input, split with white space.")
+    arg_parser.add_argument("-f", "--files", type=str, nargs="+", help="(Unsupported now)\nUse files as input, split with white space.")
+    arg_parser.add_argument("-o", "--output", type=str, nargs="+", default="json", help="Output with [json|csv|txt] format. Can be multiple values. Default: json.")
+    arg_parser.add_argument("-t", "--type", type=str, nargs=1, default="A", help="Resolve the specific record type. Default: A")
+    arg_parser.add_argument("-s", "--save", action="store_true", help="(Unsupported now)\nSave output with specific format as file with filename same as hostnames.")
+    arg_parser.add_argument("-d", "--deduplicate", action="store_true", help="Remove the duplicated values in result with txt format.")
+    arg_parser.add_argument("-v", "--version", action="version", version="akdig v0.1-Alpha")
+    # args = arg_parser.parse_args()
+    # args = arg_parser.parse_args("-h".split())
+    # args = arg_parser.parse_args("-i www.akamai.com www.akamai.cn".split())
+    # args = arg_parser.parse_args("-i www.akamai.com www.akamai.cn -o txt csv json".split())
+    # args = arg_parser.parse_args("-i www.akamai.com www.akamai.cn -o txt csv json -s".split())
+    # args = arg_parser.parse_args("-i www.akamai.com www.akamai.cn -o txt csv json -s -d".split())
+    # args = arg_parser.parse_args("-i www.akamai.com www.akamai.net -o txt csv -d".split())
+    args = arg_parser.parse_args("-i www.akamai.com.cn -o txt -d".split())
+
+    # Get all the hostnames
+    if (args.inputs or args.files):
+        if args.inputs:
+            output_result = process_input_std(hostnames=args.inputs, dns_type=args.type)
+        elif args.files:
+            output_result = process_input_files(files=args.files, dns_type=args.type)
+
+        # Save to file or not
+        if args.save:
+            # Save to file if true
+            process_result = process_output_file(output_result=output_result, output_formats=args.output, output_deduplicate=args.deduplicate)
+            process_result_json = json.dumps(process_result, ensure_ascii=False, indent=4)
+            print(process_result_json)
+        else:
+            # Or use standard output
+            process_output_std(output_result=output_result, output_formats=args.output, output_deduplicate=args.deduplicate)
---
+++ 06_dig/self_dnschekcer.org_DNSs.md
@@ -0,0 +1,142 @@
+# DNS of dnschecker.org
+
+- URL: <view-source:https://dnschecker.org/>
+- Lines: 1039 - 1069
+- Regex:
+  - Match: `^<tr.+data-location="([^"]+)".+data-provider="([^"]+)".+(\n.+){2}data-clipboardtext="([^"]+)".+(\n.+){4}`
+  - Replacement: `Location: $1\nProvider: $2\nDNS: $4\n`
+- Result:
+
+```
+Location: Mountain View CA, United States
+Provider:  Google
+DNS: 8.8.8.8
+
+Location: Berkeley, US
+Provider: Quad9
+DNS: 9.9.9.9
+
+Location: San Francisco CA, United States
+Provider: OpenDNS
+DNS: 208.67.222.220
+
+Location: Miami, United States
+Provider: AT&amp;T Services
+DNS: 12.121.117.201
+
+Location: Canoga Park, CA, United States
+Provider:  Sprint
+DNS: 204.117.214.10
+
+Location: Dothan, United States
+Provider: Comodo Secure DNS
+DNS: 8.26.56.26
+
+Location: San Francisco, US
+Provider: Quad9
+DNS: 149.112.112.112
+
+Location: St. John's, Canada
+Provider: Memorial University of Newfoundland
+DNS: 134.153.233.140
+
+Location: Yekaterinburg, Russian Federation
+Provider:  Skydns
+DNS: 195.46.39.39
+
+Location: Cullinan, South Africa
+Provider: Liquid Telecommunications Ltd
+DNS: 5.11.11.5
+
+Location: Roosendaal, Netherlands
+Provider: NForce Entertainment B.V.
+DNS: 185.107.80.84
+
+Location: Paris, France
+Provider: Online S.A.S.
+DNS: 163.172.107.158
+
+Location: Madrid, Spain
+Provider: Prioritytelecom Spain S.A.
+DNS: 212.230.255.1
+
+Location: Zizers, Switzerland
+Provider: Oskar Emmenegger
+DNS: 194.209.157.109
+
+Location: Innsbruck, Austria
+Provider: nemox.net
+DNS: 83.137.41.9
+
+Location: Manchester, United Kingdom
+Provider: Ancar B Technologies Ltd
+DNS: 194.145.240.6
+
+Location: Copenhagen, Denmark
+Provider: Tele Danmark
+DNS: 80.196.100.209
+
+Location: Oberhausen, Germany
+Provider: Verizon Deutschland GmbH
+DNS: 194.172.160.4
+
+Location: Tlalnepantla, Mexico
+Provider: Uninet S.A. de C.V.
+DNS: 148.235.82.66
+
+Location: Santa Cruz do Sul, Brazil
+Provider: Claro S.A
+DNS: 200.248.178.54
+
+Location: Kuala Lumpur, Malaysia
+Provider: Ohana Communications Sdn Bhd
+DNS: 103.26.250.4
+
+Location: Research, Australia
+Provider: Cloudflare Inc
+DNS: 1.1.1.1
+
+Location: Melbourne, Australia
+Provider: Pacific Internet
+DNS: 61.8.0.113
+
+Location: Auckland, New Zealand
+Provider: SiteHost
+DNS: 223.165.64.97
+
+Location: Singapore
+Provider: T-systems Singapore Pte
+DNS: 202.56.128.30
+
+Location: Seoul, South Korea
+Provider: LG Dacom Corporation
+DNS: 164.124.101.2
+
+Location: Hangzhou, China
+Provider: Aliyun Computing Co. Ltd
+DNS: 223.5.5.5
+
+Location: Izmir, Turkey
+Provider: Turk Telekom
+DNS: 212.175.192.114
+
+Location: Coimbatore, India
+Provider: Skylink Fibernet Private Limited
+DNS: 103.99.150.10
+
+Location: Islamabad, Pakistan
+Provider: CMPak Limited
+DNS: 209.150.154.1
+
+Location: Viana do Castelo, Portugal
+Provider: CLOUDITY Network
+DNS: 185.83.212.30
+
+Location: Ireland
+Provider: Daniel Cid
+DNS: 185.228.168.9
+
+Location: Kaharole, Bangladesh
+Provider: Md Masud Rana Roni
+DNS: 103.157.237.34
+```
