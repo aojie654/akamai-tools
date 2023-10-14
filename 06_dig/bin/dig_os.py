edbe65510b0dd0cd9203cd188793927ce14c55ef
aojie654 <aojie654@live.cn>
2023-10-14 13:15:20 +0800

    TMP

--- 06_dig/bin/dig_os.py
+++ 06_dig/bin/dig_os.py
@@ -5,6 +5,7 @@ dig with multiple DNS
 通过多个 DNS 进行解析
 """
 
+import subprocess
 import argparse
 import csv
 import json
@@ -90,9 +91,12 @@ def resolve_hostname(hostname, dns_type):
             output_result_hostname[dns_server] = dns_dict[dns_server]
             dns_resolver.nameservers = [dns_server]
             try:
-                dns_response = dns_resolver.resolve(qname=hostname, rdtype=dns_type, lifetime=1)
-                if dns_response.rrset:
-                    dns_resolve_result = dns_response.rrset.to_text().split()[-1]
+                dns_cmd = "dig +short {:} -t {:} @{:}".format(hostname, dns_type, dns_server)
+                dns_response = subprocess.check_output(dns_cmd)
+                dns_resolve_result = dns_response.decode("utf-8")
+                # dns_response = dns_resolver.resolve(qname=hostname, rdtype=dns_type, lifetime=1)
+                # if dns_response.rrset:
+                #     dns_resolve_result = dns_response.rrset.to_text().split()[-1]
             except Exception as expt:
                 dns_resolve_result = "Exception: {:}".format(str(expt))
             output_result_hostname[dns_server]["Result"] = dns_resolve_result
@@ -395,10 +399,11 @@ if __name__ == "__main__":
     arg_parser.add_argument("-s", "--save", action="store", default=False, help="Save output with specific format as file, 'h' to same as hostname, and other to filename.")
     arg_parser.add_argument("-t", "--type", action="store", default="A", help="Resolve the specific record type. Default: A.")
     arg_parser.add_argument("-v", "--version", action="version", version=load_version())
-    args = arg_parser.parse_args()
+    # args = arg_parser.parse_args()
     # __DEBUG_FLAG__: inputs
+    args_str = "-i www.aojie654.com www.akasao.com -t CNAME -o txt csv json -d"
     # args_str = "-i www.aojie654.com www.akasao.com -t CNAME -o txt csv json -d -s h"
-    # args = arg_parser.parse_args(args_str.split())
+    args = arg_parser.parse_args(args_str.split())
 
     # Set output_delimiter
     var_delimiter_character = args.character
